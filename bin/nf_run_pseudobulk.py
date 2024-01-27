39956cdf1e936633824e4846973db20c42047912
clarisse-lau <clarisse.lau@isbscience.org>
2024-01-26 16:10:57 -0800

    remove htan references

--- bin/nf_run_pseudobulk.py
+++ bin/nf_run_pseudobulk.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import sys
 import scanpy as sc
 import pandas as pd
@@ -7,38 +8,33 @@ import decoupler as dc
 import synapseclient
 from synapseclient import File
 
-#vals:
-#synapse id for input h5ad file
-#name in obs with sample id
-#name in obs with cell type id
-#name of layer with raw counts - default counts
-#mode - default sum
-
-#Example: python run_pseudobulk.py syn52118064 HTAN_Biospecimen_ID cell_type_broad counts mskCimmunecells_pseudobulk_iatlas.csv syn52077539
-
-def syn_login():
-    syn = synapseclient.Synapse()
-    syn.login()
-    return syn
 
 def main():
 
-    syn = synapseclient.login()
+    syn = synapseclient.Synapse()
+    syn.login()
+
 	#arguments
-    h5ad_file = sys.argv[1]  # "syn52087127" h5ad file with preprocessed data
-    biospecimen_col = sys.argv[2] #HTAN_Biospecimen_ID
-    cell_type_col = sys.argv[3] #cell_type_broad
+    h5ad_file = sys.argv[1]  #  h5ad file with preprocessed data
+    biospecimen_col = sys.argv[2] 
+    cell_type_col = sys.argv[3] 
     counts_layer = sys.argv[4]
     
     #load data
     print("Downloading data...")
     adata_synapse = syn.get(h5ad_file)
-    #print("Loading data...")
+
+    print("Loading data...")
     adata = sc.read_h5ad(adata_synapse.path)
 
 	#Run pseudobulk
     print("Running pseudobulk...")
-    pdata = dc.get_pseudobulk(adata, sample_col = biospecimen_col, groups_col = cell_type_col, layer= counts_layer, mode='sum')
+    pdata = dc.get_pseudobulk(
+        adata, 
+        sample_col = biospecimen_col, 
+        groups_col = cell_type_col, 
+        layer= counts_layer, 
+        mode='sum')
     
     pseudobulkResults = (pdata.to_df(layer = "psbulk_props"))
     pdata.to_df(layer = "psbulk_props").to_csv('pseudobulk_results.csv')
--- bin/nf_run_scrna_pseudobulk_gsea.py
+++ bin/nf_run_scrna_pseudobulk_gsea.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import sys
 import pandas as pd
 import decoupler as dc
@@ -7,21 +8,27 @@ import synapseclient
 from synapseclient import File
 
 def file_upload(syn, table, dataset_name, export_name, upload_location):
+    '''
+    Upload output file to Synapse
+    '''
     upload_name = "_".join([dataset_name,export_name])
     table.to_csv(upload_name)
     file_entity = synapseclient.File(upload_name, parent = upload_location)
+    
     syn.store(file_entity)
 
 def main():
 
-    syn = synapseclient.login()
-	#arguments
-    pseudobulk_file = sys.argv[1]  # syn52118084 csv pseudobulk file
-    geneset_file = sys.argv[2] # syn52138713
-    dataset_name = sys.argv[3]
-    upload_location = sys.argv[4]  # syn52142300
+    syn = synapseclient.Synapse()
+    syn.login()
 
-    #load data
+	# arguments
+    pseudobulk_file = sys.argv[1]  # csv pseudobulk file
+    geneset_file = sys.argv[2] 
+    dataset_name = sys.argv[3]
+    upload_location = sys.argv[4]  
+
+    # load data
     print("Downloading data...")
     adata_synapse = syn.get(pseudobulk_file)
     geneset_synapse = syn.get(geneset_file)
@@ -30,11 +37,12 @@ def main():
     ps_data = pd.read_csv(adata_synapse.path, index_col = 0)
     reactome = pd.read_csv(geneset_synapse.path)
 
-    #adding col with cell type information
+    # add column with cell type information
     ps_data['group'] = ps_data.index.str.rsplit('_', n=1).str.get(-1)
 
     # let's run gsea for the sum of gene expression values each cell type
     print("Running GSEA for sum of cell types")
+
     sum_scores, sum_norm, sum_pvals = dc.run_gsea(
         mat = ps_data.groupby('group').sum(), 
         net=reactome,
@@ -44,6 +52,7 @@ def main():
 
     # let's run gsea for all samples x cell_type
     print("Running GSEA for each pseudobulk sample")
+
     scores, norm, pvals = dc.run_gsea(
         mat = ps_data.drop(['group'], axis=1),
         net=reactome,
@@ -51,7 +60,9 @@ def main():
         target="Gene",
     )
 
+    # concatenate and upload gsea output files
     print("Done! Saving and uploading files...")
+
     file_upload(syn, pd.concat([sum_scores, scores]), dataset_name, "gsea_scores.csv", upload_location)
     file_upload(syn, pd.concat([sum_norm, norm]),  dataset_name, "gsea_norm.csv", upload_location)
     file_upload(syn, pd.concat([sum_pvals, pvals]),  dataset_name, "gsea_pvals.csv", upload_location)
